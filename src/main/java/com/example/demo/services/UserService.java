27fa51104656d7939724e951d5ed61f1a0b3d361
JuanOneD <Juandiego.jd42@gmail.com>
2024-11-12 11:12:34 -0300

    AAA

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,10 +1,13 @@
 package com.example.demo.services;
 
+import java.util.List;
+
 import com.example.demo.model.User;
 
 public interface UserService {
     boolean checkPassword(String Password);
     User createUser(String edv,String email,String Password);
-    User getUsers(Integer page,Integer size);
+    List<User> getUsers(String email,String edv,Integer page,Integer size); /// Email e edv são da query e podem ser opcionais(null)
     User authUsers(String login,String password);
+    User getById(long id); /// QUando tiver o jwt retorna o user só pe id
 }
