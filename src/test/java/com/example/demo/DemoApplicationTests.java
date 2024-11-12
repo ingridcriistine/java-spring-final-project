a5946ac5e1551e787e31cbe3db780b7cb57a3c1c
ingridcriistine <ingridrochacristine@gmail.com>
2024-11-12 11:33:10 -0300

    updates

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -7,31 +7,32 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.demo.model.User;
+import com.example.demo.services.SpaceService;
 import com.example.demo.services.UserService;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Autowired
-	UserService service;
+	UserService userService;
+
+	@Autowired
+	SpaceService spaceService;
 
 	@Test
-    void validateInvalidPasswords() {
-        assertEquals(service.checkPassword(""), false); //senha vazia
-        assertEquals(service.checkPassword("abcd"), false); //não possui 12 caracteres
-        assertEquals(service.checkPassword("123456789123"), false); //só possui números
-        assertEquals(service.checkPassword(".,~´.;,.~´%%"), false); //possui caracteres especiais
-        assertEquals(service.checkPassword("minhasenhaaaa"), false); //só possui letras
-        assertEquals(service.checkPassword("minhasenha..."), false); //possui caracteres especiais
-        assertEquals(service.checkPassword("minhasenha123"), false); //não possui letra maiúscula
-        assertEquals(service.checkPassword("12345678910%%"), false); //não possui letras
-        assertEquals(service.checkPassword("12345678AAAAA"), false); //não possui letras minúsculas
-        assertEquals(service.checkPassword("12345678aaaaa"), false); //não possui letras maiúsculas
-    }
-
-    @Test
-    void validateValidPasswords() {
-        assertEquals(service.checkPassword("MinhaSenha12"), true); //tem 12 caracteres, letra maiúscula e minúscula
+    void validatePassword() {
+        assertEquals(userService.checkPassword(""), false); //senha vazia
+        assertEquals(userService.checkPassword("abcd"), false); //não possui 12 caracteres
+        assertEquals(userService.checkPassword("123456789123"), false); //só possui números
+        assertEquals(userService.checkPassword(".,~´.;,.~´%%"), false); //possui caracteres especiais
+        assertEquals(userService.checkPassword("minhasenhaaaa"), false); //só possui letras
+        assertEquals(userService.checkPassword("minhasenha..."), false); //possui caracteres especiais
+        assertEquals(userService.checkPassword("minhasenha123"), false); //não possui letra maiúscula
+        assertEquals(userService.checkPassword("12345678910%%"), false); //não possui letras
+        assertEquals(userService.checkPassword("12345678AAAAA"), false); //não possui letras minúsculas
+        assertEquals(userService.checkPassword("12345678aaaaa"), false); //não possui letras maiúsculas
+        assertEquals(userService.checkPassword("MinhaSenha12"), true); //tem 12 caracteres, letra maiúscula e minúscula
     }
 
 	@Test
@@ -40,9 +41,35 @@ class DemoApplicationTests {
 		String email = "user@email.com";
 		String senha = "MinhaSenha12";
 
-		assertNotEquals(service.createUser(edv, email, senha), null); //o user será criado se o retorno não for igual a null
-		assertNotEquals(service.authUsers(edv, senha), null); //será feita a autenticação do usuário se o retorno não for igual a null
-		assertNotEquals(service.getUsers(1, 5), null); 
+		assertNotEquals(userService.createUser(edv, email, senha), null); //o user será criado se o retorno não for igual a null
+		assertNotEquals(userService.authUsers(edv, senha), null); //será feita a autenticação do usuário se o retorno não for igual a null
+		assertNotEquals(userService.getUsers(email, edv, 1, 5), null); //irá retornar o user se não for igual a null
+		assertNotEquals(userService.getById(0), null); //irá retornar o user se não for igual a null
+	}
+
+	@Test
+	void validateSpace() {
+		String name = "Teste";
+		var user = new User();
+		var space = spaceService.createSpace(name, user);
+
+		assertEquals(spaceService.deleteSpace(space.getId()), true); //o space será deletado se o retorno for igual a true
+		assertNotEquals(spaceService.getSpaces(name, 1, 5), null); //irá retornar o space se não for igual a null
+	}
+
+	@Test 
+	void validatePermission() {
+		
+	}
+
+	@Test 
+	void validateQuestion() {
+
+	}
+
+	@Test 
+	void validateAnswer() {
+		
 	}
 
 
