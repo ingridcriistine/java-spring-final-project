da8febeb69de1e1748f123c8f117ad2ea8ed8661
milena579 <milecalegari@gmail.com>
2024-11-14 08:55:31 -0300

    fazendo coisas do space

--- src/main/java/com/example/demo/controllers/SpaceController.java
+++ src/main/java/com/example/demo/controllers/SpaceController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.SpaceData;
 import com.example.demo.model.Space;
+import com.example.demo.repositories.SpaceRepository;
 import com.example.demo.services.SpaceService;
 
 @RestController
@@ -15,10 +17,11 @@ public class SpaceController {
     @Autowired
     SpaceService service;
 
+    @Autowired
+    SpaceRepository repo;
 
     @PostMapping("/spaces")
-    public ResponseEntity<String> create(@RequestBody Space data){
+    public ResponseEntity<String> create(@RequestBody SpaceData data){
 
-       
     }
 }
