54946b3d26c0e414e77b03ef0d971566f3913341
WallyDev <wallace07abreu@gmail.com>
2024-11-14 09:25:25 -0300

    Dependencias arrumadas

--- src/main/java/com/example/demo/DependencyConfiguration.java
+++ src/main/java/com/example/demo/DependencyConfiguration.java
@@ -2,11 +2,16 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.example.demo.dto.Token;
+import com.example.demo.impl.DefaultJWTService;
 import com.example.demo.impl.QuestionImpl;
 import com.example.demo.impl.UserImplements;
 import com.example.demo.repositories.interfaces.UserInter;
 import com.example.demo.repositories.mocks.UserMock;
+import com.example.demo.services.JWTService;
 import com.example.demo.services.QuestionService;
 import com.example.demo.services.UserService;
 
@@ -25,5 +30,15 @@ public class DependencyConfiguration {
     @Bean
     public UserService userService(){
         return new UserImplements();
+    }
+
+        @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
+
+    @Bean
+    public JWTService<Token> jwtService() {
+        return new DefaultJWTService();
     }
 }
