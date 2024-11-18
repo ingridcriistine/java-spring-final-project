5be99bba27d4cf4e00bac090e2bd5834d699de35
Dudous <sampaioeeduardo36@gmail.com>
2024-11-18 08:02:23 -0300

    quick fixes

--- src/main/java/com/example/demo/controllers/QuestionController.java
+++ src/main/java/com/example/demo/controllers/QuestionController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.QuestionData;
@@ -18,11 +20,6 @@ import com.example.demo.dto.Token;
 import com.example.demo.repositories.PermissionRepository;
 import com.example.demo.repositories.SpaceRepository;
 import com.example.demo.services.QuestionService;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.sun.net.httpserver.HttpsConfigurator;
 
 @RestController
 @RequestMapping
